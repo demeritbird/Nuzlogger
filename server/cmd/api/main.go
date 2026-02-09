2ab8626971a855abdd7e6ac030d77e0edf1c8afa
demeritbird <lekjiewei@hotmail.sg>
2026-02-10 00:13:25 +0800

    feat: connect server to postgres db

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	repository "nuzlogger-server/internal"
+	"nuzlogger-server/internal/dbrepo"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,13 +15,11 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     repository.DatabaseRepo
 }
 
 func main() {
-	//// Set Application Config ////
-	var app application
-	app.Domain = "nuzlogger.com"
-
 	// Load env file
 	if os.Getenv("LOCAL_ENV") == "" {
 		err := godotenv.Load()
@@ -28,13 +28,31 @@ func main() {
 		}
 	}
 
+	//// Set Application Config ////
+	var app application
+	app.Domain = "nuzlogger.com"
+	app.DSN = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s timezone=UTC connect_timeout=5",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASS"),
+		os.Getenv("DATABASE_NAME"),
+		os.Getenv("DATABASE_SSLMODE"),
+	)
+
 	//// Read from Command Line ////
 
 	//// Connect to the Database ////
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 
 	//// Start a Web Server ////'
 	log.Println("Starting Server on Port", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
